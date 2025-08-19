f779b5b359f1e81c44c68753bc742559567d8fab
Roman Tihonov <sar-okna64@yandex.ru>
2025-08-19 19:13:50 +0400

    Были перепутаны файлы css и js, внесены исправления перенесен код в файл script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = "Заголовок";
+const screens = "Превью";
+let screenPrice = 100;
+const rollback = "Отменить оплату";
+const fullPrice = "Все цены";
+const adaptive = "Оптимизировать цену";
+
+alert(title);  
