5e3935030c4278bf64fbdd73a24c3ad46f2ed49c
Roman Tihonov <sar-okna64@yandex.ru>
2025-08-19 19:34:27 +0400

    очистил ветку dev

--- script.js
+++
@@ -1,8 +0,0 @@
-const title = "Заголовок";
-const screens = "Превью";
-let screenPrice = 100;
-const rollback = "Отменить оплату";
-const fullPrice = "Все цены";
-const adaptive = "Оптимизировать цену";
-
-alert(title);  
